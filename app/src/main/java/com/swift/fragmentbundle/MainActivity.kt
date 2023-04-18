91fd577741675abd582f27fba6c1e32cdf3296b0
AnuSrest <psrest789@gmail.com>
2023-04-19 05:14:09 +0545

    Shown fragment 1 and 2 in MainActivity

--- app/src/main/java/com/swift/fragmentbundle/MainActivity.kt
+++ app/src/main/java/com/swift/fragmentbundle/MainActivity.kt
@@ -2,10 +2,33 @@ package com.swift.fragmentbundle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firstFragment1 = Fragment1()
+        val secondFragment2 = Fragment2()
+        val btn_fragment = findViewById<Button>(R.id.fragment1_btn)
+        val btn_fragment2 = findViewById<Button>(R.id.fragment2_btn)
+         supportFragmentManager.beginTransaction().apply {
+             replace(R.id.frame_container, firstFragment1)
+             commit()
+         }
+        btn_fragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, firstFragment1)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        btn_fragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, secondFragment2)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
